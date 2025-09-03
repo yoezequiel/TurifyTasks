0cb618edb42b270fe34d1dfb94af4fbd8aae7b23
yoezequiel <ezequielcasiano15@gmail.com>
2025-09-03 17:41:21 -0300

    feat: Añadir configuración del servidor en astro.config.mjs y agregar imagen de fondo

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        host: true, // permite conexiones externas
+        port: 4321, // tu puerto local
+        allowedHosts: true, // permite cualquier host (LocalTunnel incluido)
+    },
+});
